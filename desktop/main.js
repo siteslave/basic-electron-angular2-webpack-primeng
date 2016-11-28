16fc772f0736fef80b53e4d09cbc93edae0b9548
Satit Rianpit <rianpit@gmail.com>
2016-11-28 11:32:55 +0700

    ปรับปรุงการ copy file

--- desktop/main.js
+++ desktop/main.js
@@ -13,7 +13,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    frame: false
+    frame: true
   })
 
   // and load the index.html of the app.
